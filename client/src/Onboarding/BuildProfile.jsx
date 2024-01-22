1c8819354fa304e5c047b0cc563559855ce286ed
Zach <zachlandis91@gmail.com>
2024-01-22 10:13:59 -0700

    Set state for each checked checkbox in BuildProfile component

--- client/src/Onboarding/BuildProfile.jsx
+++ client/src/Onboarding/BuildProfile.jsx
@@ -1,29 +1,54 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { diets, intolerances, preferredCuisines } from './profileOptions'
 
 function BuildProfile() {
     const [name, setName] = useState('');
-    const [yearOfBirth, setYearOfBirth] = useState('');
+    const [profileIntolerances, setProfileIntolerances] = useState([]);
+    const [profileDiets, setProfileDiets] = useState([])
+    const [profilePreferredCuisines, setProfilePreferredCuisines] = useState([])
 
     const currentYear = new Date().getFullYear();
     const startYear = 1920;
     const years = Array.from(new Array(currentYear - startYear + 1), (val, index) => currentYear - index);
 
-    const mappedOptions = (options) => {
+    const handleCheckboxChange = (option, setterFunction, currentState) => {
+        const currentIndex = currentState.indexOf(option);
+        let newChecked = [...currentState];
+    
+        if (currentIndex === -1) {
+            newChecked.push(option);
+        } else {
+            newChecked.splice(currentIndex, 1);
+        }
+    
+        setterFunction(newChecked);
+    };
+    
+    const mappedOptions = (options, setterFunction, currentState) => {
         return options.map((option) => (
-            <div>
+            <div key={option}>
                 <input
                     type='checkbox'
                     value={option}
+                    checked={currentState.includes(option)}
+                    onChange={() => handleCheckboxChange(option, setterFunction, currentState)}
                 />
                 <label>{option}</label>
             </div>
-        ))
-    }
+        ));
+    };
+    
 
-    const mappedDiets = mappedOptions(diets);
-    const mappedIntolerances = mappedOptions(intolerances);
-    const mappedPreferredCuisines = mappedOptions(preferredCuisines);
+    const mappedDiets = mappedOptions(diets, setProfileDiets, profileDiets);
+    const mappedIntolerances = mappedOptions(intolerances, setProfileIntolerances, profileIntolerances);
+    const mappedPreferredCuisines = mappedOptions(preferredCuisines, setProfilePreferredCuisines, profilePreferredCuisines);
+
+
+    useEffect(() => {
+        console.log("Profile Diets: ", profileDiets);
+        console.log("Profile Intolerances: ", profileIntolerances);
+        console.log("Profile Cuisines: ", profilePreferredCuisines)
+    })
 
 
     return (
