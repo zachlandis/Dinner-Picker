8f61b5c7a4c29934105aefd0649deace05afa6bf
Zach <zachlandis91@gmail.com>
2024-02-06 19:59:54 -0700

    Allow user to regenerate randomized menu

--- client/src/User/RandomizedMenu.jsx
+++ client/src/User/RandomizedMenu.jsx
@@ -1,28 +1,30 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 function RandomizedMenu({ currentUser }) {
   const [randomizedMenu, setRandomizedMenu] = useState([]);
+  const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+  const generateRandomizedMenu = () => {
+    const shuffledDinners = [...currentUser.dinner_wishlists].sort(() => Math.random() - 0.5).slice(0, 7);
+    setRandomizedMenu(shuffledDinners);
+    localStorage.setItem('randomizedMenu', JSON.stringify(shuffledDinners));
+  };
 
   useEffect(() => {
-    // Retrieve randomized menu from local storage
     const storedMenu = localStorage.getItem('randomizedMenu');
     if (storedMenu) {
       setRandomizedMenu(JSON.parse(storedMenu));
     } else {
-      // Generate and save randomized menu if not found in local storage
-      const shuffledDinners = [...currentUser.dinner_wishlists].sort(() => Math.random() - 0.5).slice(0, 7);
-      setRandomizedMenu(shuffledDinners);
-      localStorage.setItem('randomizedMenu', JSON.stringify(shuffledDinners));
+      generateRandomizedMenu();
     }
-  }, [currentUser.dinner_wishlists]);
-
-  const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  }, []); // Run once on component mount
 
   return (
     <div className='centered-content'>
       <h3>This Week's Dinners</h3>
+      <button onClick={generateRandomizedMenu}>Shuffle Menu</button>
       <table className='randomized-menu-table'>
         <thead>
           <tr className='randomized-menu-item-columns'>
